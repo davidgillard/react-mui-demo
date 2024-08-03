b1f5694742abe2239c60bd5fbc07be73acab44f1
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-03 21:02:34 +0200

    add typography

--- src/components/MuiTypography.tsx
+++ src/components/MuiTypography.tsx
@@ -7,9 +7,20 @@ export const MuiTypography = () => {
       <Typography variant='h1'>h1 Heading</Typography>
       <Typography variant='h2'>h2 Heading</Typography>
       <Typography variant='h3'>h3 Heading</Typography>
-      <Typography variant='h4'>h4 Heading</Typography>
+      <Typography variant='h4' component='h1' gutterBottom>h4 Heading</Typography>
       <Typography variant='h5'>h5 Heading</Typography>
       <Typography variant='h6'>h6 Heading</Typography>
+      
+      <Typography variant='subtitle1'>Sub title 1</Typography>
+      <Typography variant='subtitle2'>Sub title 2</Typography>
+
+      <Typography variant='body1'>
+      Lorem ipsum odor amet, consectetuer adipiscing elit. Nunc lorem dignissim nostra sem fermentum donec. Massa purus tellus rhoncus nisl nostra. Habitant cursus ultrices non; nibh montes conubia. Sollicitudin consequat vulputate velit amet quisque imperdiet eu. Blandit orci elementum enim nisl nulla magna fringilla sodales inceptos. In ultricies aenean venenatis quis sociosqu.
+      </Typography>
+      <Typography variant='body2'>
+      Dapibus turpis aliquet hac posuere aptent ullamcorper. Tempus eu turpis dictum; condimentum ipsum amet justo diam. Orci maecenas dui dictum fames a nostra inceptos. Torquent viverra augue accumsan litora integer porta est. Viverra hac eu gravida eros ultrices dis. Cursus torquent faucibus fermentum ridiculus; dictum tellus. Natoque nisl facilisis lectus mauris porttitor torquent class molestie nec? Mollis ullamcorper amet, sodales eleifend neque mus consectetur.
+      Lorem ipsum odor amet, consectetuer adipiscing elit. Nunc lorem dignissim nostra sem fermentum donec. Massa purus tellus rhoncus nisl nostra. Habitant cursus ultrices non; nibh montes conubia. Sollicitudin consequat vulputate velit amet quisque imperdiet eu. Blandit orci elementum enim nisl nulla magna fringilla sodales inceptos. In ultricies aenean venenatis quis sociosqu.
+      </Typography>
     </div>
   )
 }
