9ecae4bd531d6a0222e7a20a12af4c67517bcaf4
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-04 13:02:07 +0200

    Mise en place des groupes de boutons

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Button, IconButton } from "@mui/material";
+import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 
 export const MuiButton = () => {
@@ -31,6 +31,13 @@ export const MuiButton = () => {
         <Button variant='contained' startIcon={<SendIcon />} disableRipple onClick={() => alert('Clicked')}>Send</Button> 
         <Button variant='contained' endIcon={<SendIcon />} disableElevation>Send</Button> 
         <IconButton aria-label="send" color="success" size="small"><SendIcon/></IconButton>
+      </Stack>
+      <Stack direction='row'>
+        <ButtonGroup variant="contained" orientation="vertical" size="small" color="secondary" aria-label='alignment button group'>
+          <Button onClick={() => alert('Left clicked')}>Left</Button> 
+          <Button>Center</Button> 
+          <Button>Right</Button> 
+        </ButtonGroup>
       </Stack>
 
     </Stack>
