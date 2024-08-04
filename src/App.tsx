07056d414aaee1c618b42881290f69449f9805a7
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-04 16:57:43 +0200

    ajout du composant textfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Typography } from '@mui/material';
 import './App.css';
-import { MuiButton } from './components/MuiButton';
+// import { MuiButton } from './components/MuiButton';
+import { MuiTextField } from './components/MuiTextField';
 // import { MuiTypography } from './components/MuiTypography';
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography /> */}
-      <MuiButton />
+      {/* <MuiButton /> */}
+      <MuiTextField />
     </div>
   );
 }
